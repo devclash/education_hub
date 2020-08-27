a042f97e90128e77e5230ca5450e82c0d76599c5
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-08-27 21:03:18 +0545

    removed posts for now and added questions app

--- education_hub/urls.py
+++ education_hub/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
-    path('', views.index, name='index')
 ]
 
 # Changed some default title from django admin panel
