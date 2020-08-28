b8a413af81bcbdab98297e004205850db1106ca9
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-08-28 21:41:49 +0545

    added models for notes app

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    '''
+    Model for tags that can be attached to notes
+    '''
+    name = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    '''
+    The actual model of our notes app
+    '''
+    title = models.CharField(max_length=100)
+    decription = RichTextField(blank=True, null=True)
+    document = models.FileField(upload_to='notes/')
+    date_created = models.DateTimeField(default=timezone.now)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
